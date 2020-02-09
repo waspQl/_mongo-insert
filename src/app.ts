d9152a019ab0b895a3dd837977517c082235c6c0
sakutaro <u390862@gmail.com>
2020-02-09 19:40:00 +0900

    update: insert処理を追加

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import mongodb from 'mongodb'
 const MongoClient = mongodb.MongoClient
+const assert = require('assert');
 
 // 引数が渡ってきた場合はそれをport番号として使う
 let port = 27017
@@ -9,15 +10,41 @@ if (
     parseInt(process.argv[2])
   )
 ) port = parseInt(process.argv[2])
-// mongoへの接続確認用データベース
-const dbname = 'admin'
+const url = 'mongodb://127.0.0.1:' + port + '/'
 
-MongoClient.connect('mongodb://127.0.0.1:' + port + '/' + dbname, (err, db) => {
-  if (err === null) {
-    console.log("Connected successfully to mongo server")
-  } else {
-    console.log("Connection failure to mongo server", err)
-  }
+const dbName = 'testdb'
+const collectionName = 'testCollection'
 
-  db.close()
+// Connect using MongoClient
+MongoClient.connect(url, async (err, db) => {
+  if (err) throw err
+  const dbo = db.db(dbName)
+  const collection = dbo.collection(collectionName)
+
+  const obj = { a: '0' }
+  const ary = [
+    { a: '1' },
+    { a: '2' }
+  ]
+
+  await collection.insertOne(obj, (err, res) => {
+    if (err) throw err
+    assert.equal(1, res.insertedCount)
+    return
+  })
+
+  await collection.insertMany(ary)
+    .then(res => {
+      assert.equal(ary.length, res.insertedCount)
+    })
+    .catch(err => {
+      throw err
+    })
+
+  await collection.drop(async (err, res) => {
+    if (err) throw err
+    assert.equal(true, res)
+
+    db.close()
+  })
 })
