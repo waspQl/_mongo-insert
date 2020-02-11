0bb1a4ef0740e6d8f36076b44399cc5395595928
sakutaro <u390862@gmail.com>
2020-02-11 20:54:11 +0900

    update: bulk処理を追加

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
 import mongodb from 'mongodb'
 const MongoClient = mongodb.MongoClient
-const assert = require('assert');
+const assert = require('assert')
+const { performance } = require('perf_hooks')
 
 interface InsertOneWriteOpResult {
-    insertedCount: number;
-    ops: Array<any>;
-    insertedId: any;
-    connection: any;
-    result: { ok: number, n: number }
+  result: { ok: number, n: number }
+  ops: Array<any>
+  insertedCount: number
+  insertedId: any
+}
+
+interface BulkWriteResult {
+  ok: number
+  writeErrors: Array<any>
+  writeConcernErrors: Array<any>
+  insertedIds: Array<any>
+  nInserted: number
+  nUpserted: number
+  nMatched: number
+  nModified: number
+  nRemoved: number
+  upserted: Array<any>
 }
 
 // 引数が渡ってきた場合はそれをport番号として使う
@@ -28,13 +41,45 @@ MongoClient.connect(url, async (err, db) => {
   if (err) throw err
   const dbo = db.db(dbName)
 
-  await insertOne(dbo, (res: InsertOneWriteOpResult) => {
+  const obj = { a: '0' }
+  let startTime = performance.now()
+  await insertOne(dbo, obj, (res: InsertOneWriteOpResult) => {
     assert.equal(1, res.insertedCount)
   })
+  let endTime = performance.now()
+  console.log(
+    'insertOne performance:',
+    Math.floor(endTime - startTime),
+    'msec'
+  )
 
-  await insertMany(dbo, (res: InsertOneWriteOpResult) => {
-    assert.equal(2, res.insertedCount)
+  const aryLength = 10000
+  const ary = Array.from(new Array(aryLength))
+    .map((v, i) => i)
+    .map(val => {
+      return {a: val}
+    })
+  startTime = performance.now()
+  await insertMany(dbo, ary, (res: InsertOneWriteOpResult) => {
+    assert.equal(aryLength, res.insertedCount)
   })
+  endTime = performance.now()
+  console.log(
+    'insertMany performance:',
+    Math.floor(endTime - startTime),
+    'msec'
+  )
+
+  startTime = performance.now()
+  await bulkInsert(dbo, ary, (res: BulkWriteResult) => {
+    assert.equal(aryLength, res.nInserted)
+  })
+  endTime = performance.now()
+  console.log(
+    'bulkInsert performance:',
+    Math.floor(endTime - startTime),
+    'msec'
+  )
 
   await drop(dbo, (res: boolean) => {
     assert.equal(true, res)
@@ -43,9 +88,7 @@ MongoClient.connect(url, async (err, db) => {
 })
 
 
-const insertOne = async (db: mongodb.Db, callback: (msg: InsertOneWriteOpResult) => void) => {
-  const obj = { a: '0' }
-
+const insertOne = async (db: mongodb.Db, obj: object, callback: (msg: InsertOneWriteOpResult) => void) => {
   await db.collection(collectionName)
     .insertOne(obj, (err: Error, res: InsertOneWriteOpResult) => {
       if (err) throw err
@@ -53,12 +96,7 @@ const insertOne = async (db: mongodb.Db, callback: (msg: InsertOneWriteOpResult)
     })
 }
 
-const insertMany = async (db: mongodb.Db, callback: (msg: any) => void) => {
-  const ary = [
-    { a: '1' },
-    { a: '2' }
-  ]
-
+const insertMany = async (db: mongodb.Db, ary: Array<any>, callback: (msg: any) => void) => {
   await db.collection(collectionName)
     .insertMany(ary)
     .then(res => {
@@ -67,6 +105,18 @@ const insertMany = async (db: mongodb.Db, callback: (msg: any) => void) => {
     .catch(err => {
       throw err
     })
+}
+
+const bulkInsert = async (db: mongodb.Db, ary: Array<any>, callback: (msg: any) => void) => {
+  const bulk = db.collection(collectionName).initializeUnorderedBulkOp();
+  ary.forEach(obj => {
+    bulk.insert(obj);
+  })
+
+  await bulk.execute((err, res) => {
+    if (err) throw err
+    callback(res)
+  });
 }
 
 const drop = async (db: mongodb.Db, callback: (msg: boolean) => void) => {
