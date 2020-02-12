20c60fd147b2219d209b42b1aa89a44b75519a68
sakutaro <u390862@gmail.com>
2020-02-12 12:06:26 +0900

    add: findのチェックを追加

--- src/app.ts
+++ src/app.ts
@@ -10,16 +10,18 @@ interface InsertOneWriteOpResult {
 }
 
 interface BulkWriteResult {
-  ok: number
-  writeErrors: Array<any>
-  writeConcernErrors: Array<any>
-  insertedIds: Array<any>
-  nInserted: number
-  nUpserted: number
-  nMatched: number
-  nModified: number
-  nRemoved: number
-  upserted: Array<any>
+  result: {
+    ok: number
+    writeErrors: Array<any>
+    writeConcernErrors: Array<any>
+    insertedIds: Array<any>
+    nInserted: number
+    nUpserted: number
+    nMatched: number
+    nModified: number
+    nRemoved: number
+    upserted: Array<any>
+  }
 }
 
 // 引数が渡ってきた場合はそれをport番号として使う
@@ -35,63 +37,102 @@ const url = 'mongodb://127.0.0.1:' + port + '/'
 const dbName = 'testdb'
 const collectionName = 'testCollection'
 
+let startTime: number
+let endTime: number
+
 // Connect using MongoClient
 MongoClient.connect(url, async (err, db) => {
   if (err) throw err
   const dbo = db.db(dbName)
 
-  const obj = { a: '0' }
-  let startTime = Date.now()
+  let cnt = 0
+  const obj = { a: 0 }
+  updateStartTime()
   await insertOne(dbo, obj, (res: InsertOneWriteOpResult) => {
+    updateEndTime()
+    // console.log(res.ops)
     assert.equal(1, res.insertedCount)
+    consoleTime('insertOne Date', endTime - startTime)
   })
-  let endTime = Date.now()
-  console.log(
-    'insertOne Date:',
-    endTime - startTime,
-    'msec'
-  )
+  cnt += 1
 
-  const aryLength = 10000
-  const ary = Array.from(new Array(aryLength))
+  const aryLength = 1000000
+  const ary1 = Array.from(new Array(aryLength))
     .map((v, i) => i)
     .map(val => {
-      return {a: val}
+      return {b: val}
     })
-  startTime = Date.now()
-  await insertMany(dbo, ary, (res: InsertOneWriteOpResult) => {
+  updateStartTime()
+  await insertMany(dbo, ary1, (res: InsertOneWriteOpResult) => {
+    updateEndTime()
+    // console.log(res.ops)
     assert.equal(aryLength, res.insertedCount)
+    consoleTime('insertMany Date', endTime - startTime)
   })
-  endTime = Date.now()
-  console.log(
-    'insertMany Date:',
-    endTime - startTime,
-    'msec'
-  )
+  cnt += ary1.length
 
-  startTime = Date.now()
-  await bulkInsert(dbo, ary, (res: BulkWriteResult) => {
-    assert.equal(aryLength, res.nInserted)
+  const ary2 = Array.from(new Array(aryLength))
+    .map((v, i) => i)
+    .map(val => {
+      return {c: val}
+    })
+  updateStartTime()
+  await bulkInsert(dbo, ary2, (res: BulkWriteResult) => {
+    updateEndTime()
+    // console.log(res.result.insertedIds)
+    assert.equal(aryLength, res.result.nInserted)
+    consoleTime('bulkInsert Date', endTime - startTime)
   })
-  endTime = Date.now()
-  console.log(
-    'bulkInsert Date:',
-    endTime - startTime,
-    'msec'
-  )
+  cnt += ary2.length
 
-  await drop(dbo, (res: boolean) => {
-    assert.equal(true, res)
-    db.close()
+  updateStartTime()
+  await find(dbo, async (res: Array<object>) => {
+    updateEndTime()
+    assert.equal(cnt, res.length)
+    consoleTime('find Date', endTime - startTime)
+
+    await drop(dbo, (res: boolean) => {
+      assert.equal(true, res)
+      db.close()
+    })
   })
 })
 
+const updateStartTime = () => {
+  startTime = Date.now()
+}
+
+const updateEndTime = () => {
+  endTime = Date.now()
+}
+
+const consoleTime = (name: string, processingTime: number) => {
+  const len = 16 - name.length
+
+  console.log(
+    name + (' '.repeat(len) + ':').slice(-1 * len),
+    processingTime,
+    'msec'
+  )
+}
+
+const find = async (db: mongodb.Db, callback: (msg: any) => void) => {
+  return await db.collection(collectionName)
+    .find({ })
+    .toArray((err, res) => {
+      if (err) throw err
+      callback(res)
+    })
+}
 
 const insertOne = async (db: mongodb.Db, obj: object, callback: (msg: InsertOneWriteOpResult) => void) => {
   await db.collection(collectionName)
-    .insertOne(obj, (err: Error, res: InsertOneWriteOpResult) => {
-      if (err) throw err
+    .insertOne(obj)
+    .then(res => {
       callback(res)
+    })
+    .catch(err => {
+      throw err
     })
 }
 
